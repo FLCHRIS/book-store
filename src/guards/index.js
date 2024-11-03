d3387819fe8f0e9ac5a3cf15dcb1e37215dca27f
Christian <filetelopezchristian@gmail.com>
2024-11-03 15:14:23 -0600

    New method IS_AUTHENTICATED have been created.

---
+++ src/guards/index.js
@@ -0,0 +1,22 @@
+import { useUserStore } from "../stores/user";
+
+export const isAuthenticated = (redirectRoute) => {
+  return async (to, from, next) => {
+    try {
+      const { validateSession } = useUserStore();
+
+      if (to.matched.some((record) => record.meta.requiresAuth)) {
+        const isAuthenticated = await validateSession();
+        if (!isAuthenticated) {
+          next({ name: redirectRoute });
+        } else {
+          next();
+        }
+      } else {
+        next();
+      }
+    } catch (error) {
+      next({ name: redirectRoute });
+    }
+  };
+};
