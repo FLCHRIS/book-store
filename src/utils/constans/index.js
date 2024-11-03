1f27b547b0c55f51ea6769270f120d48083872da
Christian <filetelopezchristian@gmail.com>
2024-11-03 15:24:05 -0600

    Formated file

--- src/utils/constans/index.js
+++ src/utils/constans/index.js
@@ -1,5 +1,5 @@
-export const BASE_URL = "http://localhost:3000/api";
+export const BASE_URL = 'http://localhost:3000/api'
 export const USER_ROLES = {
-  admin: "ADMIN",
-  customer: "CUSTOMER",
-};
+	admin: 'ADMIN',
+	customer: 'CUSTOMER',
+}
