57fcdcc4cf5ca97d844c4aec27aa47998386bae0
Christian <filetelopezchristian@gmail.com>
2024-11-03 15:14:40 -0600

    Created file

---
+++ src/components/NavBar.vue
@@ -0,0 +1,9 @@
+<template>
+  <nav class="bg-white border-b border-gray-200 px-6 py-3">
+    <div class="w-full max-w-[1400px] mx-auto">
+      <p class="text-xl font-semibold text-gray-900">
+        Book Store
+      </p>
+    </div>
+  </nav>
+</template>
