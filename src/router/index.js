e5e673afd670a50711264addd9497b97fdc3b894
Christian <filetelopezchristian@gmail.com>
2024-11-03 15:25:25 -0600

    Formated file

--- src/router/index.js
+++ src/router/index.js
@@ -1,34 +1,34 @@
-import { createRouter, createWebHistory } from "vue-router";
-import { isAuthenticated } from "../guards";
+import { createRouter, createWebHistory } from 'vue-router'
+import { isAuthenticated } from '../guards'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      redirect: "/admin",
-    },
-    {
-      path: "/admin",
-      component: () => import("../layouts/admin/MainLayout.vue"),
-      beforeEnter: isAuthenticated("admin-login"),
-      children: [
-        {
-          path: "",
-          name: "admin-login",
-          component: () => import("../pages/admin/LoginPage.vue"),
-        },
-        {
-          path: "home",
-          name: "admin-home",
-          meta: {
-            requiresAuth: true,
-          },
-          component: () => import("../pages/admin/HomePage.vue"),
-        },
-      ],
-    },
-  ],
-});
+	history: createWebHistory(import.meta.env.BASE_URL),
+	routes: [
+		{
+			path: '/',
+			redirect: '/admin',
+		},
+		{
+			path: '/admin',
+			component: () => import('../layouts/admin/MainLayout.vue'),
+			beforeEnter: isAuthenticated('admin-login'),
+			children: [
+				{
+					path: '',
+					name: 'admin-login',
+					component: () => import('../pages/admin/LoginPage.vue'),
+				},
+				{
+					path: 'home',
+					name: 'admin-home',
+					meta: {
+						requiresAuth: true,
+					},
+					component: () => import('../pages/admin/HomePage.vue'),
+				},
+			],
+		},
+	],
+})
 
-export default router;
+export default router
