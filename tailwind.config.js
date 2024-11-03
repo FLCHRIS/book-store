8d8d0136b55afd2ef9836de910479990fcded39e
Christian <filetelopezchristian@gmail.com>
2024-11-03 15:05:27 -0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter Variable', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+}
+
